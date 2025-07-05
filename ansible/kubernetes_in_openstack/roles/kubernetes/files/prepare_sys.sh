88656fc3ca772bf7a74de3a982d16e7d0905a066
Mei Rizal <meriz.rizal@gmail.com>
2025-07-05 18:12:26 +0700

    feat: Kubernetes in OpenStack - initial commit

---
+++ ansible/kubernetes_in_openstack/roles/kubernetes/files/prepare_sys.sh
@@ -0,0 +1,13 @@
+swapoff -a
+sed -i '/ swap / s/^/#/' /etc/fstab
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
